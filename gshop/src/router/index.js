75a43fa7dfa3578f68919655925da1de2f135b78
Woxiang9101 <1834250342@qq.com>
2019-10-19 17:57:56 +0800

    与后台接口刚刚写好之后

--- gshop/src/router/index.js
+++ gshop/src/router/index.js
@@ -6,6 +6,7 @@ import Msite from '../pages/Msite/Msite.vue'
 import Search from '../pages/Search/Search.vue'
 import Order from '../pages/Order/Order.vue'
 import Profile from '../pages/Profile/Profile.vue'
+import Login from '../pages/Login/Login.vue'
 
 Vue.use(VueRouter);
 export default new VueRouter({
@@ -17,18 +18,34 @@ export default new VueRouter({
     {
       path: '/msite',
       component: Msite,
+      meta: {
+        showFooter: true
+      }
     },
     {
       path: '/search',
       component: Search,
+      meta: {
+        showFooter: true
+      }
     },
     {
       path: '/order',
       component: Order,
+      meta: {
+        showFooter: true
+      }
     },
     {
       path: '/profile',
       component: Profile,
+      meta: {
+        showFooter: true
+      }
+    },
+    {
+      path: '/login',
+      component: Login
     }
   ]
 })
